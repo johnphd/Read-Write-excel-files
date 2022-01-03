05c8f2ecf0727f76b7d08eda78aab93042b0a3b6
John O <71325931+johnphd@users.noreply.github.com>
2022-01-03 11:19:54 +0100

    Update Read and plot excel (multipage Neware).py

--- Read and plot excel (multipage Neware).py
+++ Read and plot excel (multipage Neware).py
@@ -9,18 +9,17 @@ This is code borrowed and pieced together from github.  It's a first trial for d
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from pathlib import Path
 
 #  this section is for each data file being read into python.  There is no limit I think
-loc1 = r"C:\Temp\Matlab\practice\Data for figures (mishmash)\BiP (0.1M sodiated in G2) vs TEMPO (PB) Beta Al2O3\210508-2 END.xls" # full path
+loc1 = r"drive:\path\file.xls" # full path
 
 filename=Path(loc1).stem  # this removes the path (in loc1) from the filename
 str_1=filename              #  Identifies 'filename'as a string
 
-df = pd.concat(pd.read_excel(loc1, sheet_name=[3,4,5,6]), ignore_index=False)
-df.shape
+df = pd.concat(pd.read_excel(loc1, sheet_name=[3,4,5,6]), ignore_index=False) # sheetnumbers are manually entered, did not find a shortcut for this
+df.shape                                                # concantenates multiple pages
 
-df['Absolute Time'] = pd.to_datetime(
+df['Absolute Time'] = pd.to_datetime(   
     df['Absolute Time'],
     format='%m.%d.%Y %H:%M:%S.%f',
     errors='coerce')
@@ -29,16 +28,14 @@ df['Absolute Time'] = pd.to_datetime(
 
 origin = df['Absolute Time'].min()
 
-df['Time(h)'] = (df['Absolute Time'] - origin).dt.total_seconds() / 3600
+df['Time(h)'] = (df['Absolute Time'] - origin).dt.total_seconds() / 3600   # converts columns of time stamps to hours
 
-#  df.drop_duplicates(subset=['Absolute Time']) # this omits duplicate entries in the x axis (Time)
-
-df.plot(x='Time(h)',y='Voltage(V)', color="red")#x="Time(h)",y="overpotential(V)")
+df.plot(x='Time(h)',y='Voltage(V)', color="red")#x="Time(h)",y="overpotential(V)") # using column headers, sets x,y data and plot
 
 plt.xlim(0,1000)       # sets x axis boundaries
 plt.ylim(1.5,4)      # sets y axis boundaries
 
-plt.xlabel("time (h)")   #  This is a specific value
+plt.xlabel("time (h)")   #  explicite label for x and y axes.
 plt.ylabel("voltage (V)")  
-plt.title(str_1)        # filename is used in each sample as a graph title
+plt.title(str_1)        # filename is used in each sample as a graph title, helps when there are hundreds of graphs
 plt.show()  
